0c980aff0782949837002219c4a866e298ec1748
Zen Gunawardhana <zentagfw@gmail.com>
2022-06-11 23:18:57 -0400

    first commit v2

---
+++ iUrn/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./screens/*.tsx","./components/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: require('tailwind-rn/unsupported-core-plugins'),
+}
