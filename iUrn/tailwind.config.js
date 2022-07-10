65187cdcbe1647d1dfb58ad3e1640b0a8c2c8133
zentag <zentagfw@gmail.com>
2022-07-10 14:13:18 -0400

    🎨 DESIGN: color pallette

--- iUrn/tailwind.config.js
+++ iUrn/tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ["./screens/*.tsx","./components/*.tsx"],
   theme: {
     extend: {},
+    colors:{
+      "dark-primary": "#00066d",
+      "dark-secondary": "#000000",
+      "dark-tertiary": "#000ab5",
+      "light-primary": "#949aff",
+      "light-secondary": "#444eff",
+      "light-tertiary": "#ffffff"
+    },
   },
   plugins: [],
   corePlugins: require('tailwind-rn/unsupported-core-plugins'),
