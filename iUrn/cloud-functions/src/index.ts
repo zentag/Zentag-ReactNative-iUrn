459aa53402e09ed0c3859969e45f407507e33911
zentag <zentagfw@gmail.com>
2023-02-12 14:01:28 -0500

    🔥 FEATURE: cloud function that deletes storage files when the corresponding document is deleted

--- iUrn/cloud-functions/src/index.ts
+++ iUrn/cloud-functions/src/index.ts
@@ -1,9 +1,23 @@
 import * as functions from "firebase-functions";
+import { getStorage } from "firebase-admin/storage";
+import { initializeApp } from "firebase-admin/app";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
+initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+export const deleteImageOnDocumentDelete = functions.firestore
+  .document("Pages/{userID}/MemoryVault/{deletedDoc}")
+  .onDelete((snap) => {
+    const storage = getStorage().bucket();
+    const deletedData = snap.data();
+    const file = storage.file(deletedData?.ImageSource);
+    file
+      .delete()
+      .then(() => {
+        return console.log("successful");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
