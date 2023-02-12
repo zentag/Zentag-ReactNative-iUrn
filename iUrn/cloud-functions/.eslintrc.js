c7616789ae6bd5a08cb5d5e2ad5f2a774a8f8c08
zentag <zentagfw@gmail.com>
2023-02-12 09:07:56 -0500

    🚧 DEVELOPMENT: cloud-functions ready for development

--- iUrn/cloud-functions/.eslintrc.js
+++ iUrn/cloud-functions/.eslintrc.js
@@ -28,5 +28,9 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    "linebreak-style": 0,
+    "max-len": 0,
+    "object-curly-spacing": 0,
+    "indent": 0,
   },
 };
