a20ef8211776be3aebf54e463655c8f78c105574
zentag <zentagfw@gmail.com>
2023-02-26 12:16:20 -0500

    🐞 BUGFIX: Improve loading time

--- iUrn/hooks/useCachedResources.ts
+++ iUrn/hooks/useCachedResources.ts
@@ -14,7 +14,6 @@ export default function useCachedResources() {
 
         // Load fonts
         await Font.loadAsync({
-          ...FontAwesome.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
         });
       } catch (e) {
