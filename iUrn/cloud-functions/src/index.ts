9d209e5fbf72c81378b3fd70e28ecc83c9d77f19
zentag <38821230+zentag@users.noreply.github.com>
2023-12-10 01:41:15 +0000

    functions

--- iUrn/cloud-functions/src/index.ts
+++ iUrn/cloud-functions/src/index.ts
@@ -1,11 +1,19 @@
 import * as functions from "firebase-functions";
 import { getStorage } from "firebase-admin/storage";
+import { getFirestore } from "firebase-admin/firestore";
 import { initializeApp } from "firebase-admin/app";
-
+import { v4 as uuidv4 } from "uuid"
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 initializeApp();
-
+export const addUserDocument = functions.auth.user().onCreate((user) => {
+  const uuid = uuidv4()
+  const firestore = getFirestore()
+  const userDoc = firestore.doc(`/Users/${user.uid}`)
+  const pageDoc = firestore.doc(`/Users/${uuid}`)
+  userDoc.set({Nickname:"Set your name on your page",Pages:[uuid]})
+  pageDoc.set({Owner:user.uid})
+})
 export const deleteImageOnDocumentDelete = functions.firestore
   .document("Pages/{userID}/MemoryVault/{deletedDoc}")
   .onDelete((snap) => {
